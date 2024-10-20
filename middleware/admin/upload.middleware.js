f5fc31c8707e423a400bdbb3048593f30da4ab87
thanhlamcode <lamdoan1122334455@gmail.com>
2024-10-20 21:22:17 +0700

    Cấu hình upload file

--- middleware/admin/upload.middleware.js
+++ middleware/admin/upload.middleware.js
@@ -1,9 +1,22 @@
-const uploadToCloudinary = require("../../helpers/uploadCloudDinary");
+const streamUpload = require("../../helpers/uploadCloudDinary");
 
-module.exports.upload = async (req, res, next) => {
-  if (req.file) {
-    const link = await uploadToCloudinary(req.file.buffer);
-    req.body[req.file.fieldname] = link;
-  }
-  next();
+// Middleware xử lý upload ảnh
+const uploadMiddleware = (fieldName) => {
+  return async (req, res, next) => {
+    if (req.files && req.files[fieldName]) {
+      // Kiểm tra nếu có file với tên fieldName
+      try {
+        const fileBuffer = req.files[fieldName].data;
+        const result = await streamUpload(fileBuffer); // Upload file lên Cloudinary
+        req.uploadedFileUrl = result.secure_url; // Lưu URL của file upload vào request
+        next(); // Tiếp tục xử lý
+      } catch (error) {
+        return res.status(500).json({ error: "Lỗi khi upload file" });
+      }
+    } else {
+      next(); // Nếu không có file, tiếp tục xử lý
+    }
+  };
 };
+
+module.exports = uploadMiddleware;
--- routes/admin/information.route.js
+++ routes/admin/information.route.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../../controller/admin/information.controller");
+const uploadMiddleware = require("../../middleware/admin/upload.middleware");
 
 router.get("/:id", controller.index);
-router.patch("/edit/:id", controller.edit);
+router.patch("/edit/:id", uploadMiddleware("avatar"), controller.edit);
 
 module.exports = router;
