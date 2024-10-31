16054cd62384a2c3a293738cff099df53259704f
thanhlamcode <lamdoan1122334455@gmail.com>
2024-10-31 14:16:51 +0700

    deleteContact

--- controller/admin/contact.controller.js
+++ controller/admin/contact.controller.js
@@ -1,12 +1,33 @@
 const Contact = require("../../models/contact.model");
 
-// [DELETE] /admin/contact
+// [GET] /admin/contact
 module.exports.get = async (req, res) => {
   try {
     const id = req.params.id;
     const contact = await Contact.findOne({ _id: id });
 
     res.json(contact);
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      code: 400,
+      messgae: "Đã có lỗi xảy ra",
+    });
+  }
+};
+
+// [DELETE] /admin/contact/:contactId/:itemId
+module.exports.deleteContact = async (req, res) => {
+  try {
+    const { contactId, itemId } = req.params;
+    await Contact.updateOne(
+      { _id: contactId },
+      {
+        $pull: { contacts: { _id: itemId } },
+      }
+    );
+
+    res.json(true);
   } catch (error) {
     console.log(error);
     return res.json({
--- routes/admin/contact.route.js
+++ routes/admin/contact.route.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controller = require("../../controller/admin/contact.controller");
 
 router.get("/:id", controller.get);
+router.delete("/:contactId/:itemId", controller.deleteContact);
 
 module.exports = router;
